d17368701a63259c422310696366987c5acb168f
lauarango <88212130+lauarango@users.noreply.github.com>
2024-10-15 06:51:23 +0000

    push

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-# options to make web scraping easier
-def get_drver():
-  options = webdriver.ChromeOptions()
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--start-maximized')
-  options.add_argument('--disable-infobars')
-  options.add_argument('--disable-extensions')
-  options.add_argument('--disable-blink-features=AutomationControlled')
+def get_drvier():
+    # Set options to make browsing easier
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+    options.add_experimental_option("detach", True)
 
-  #specify the url
-  drver = webdriver.Chrome(options=options)
-  drver.get("http://automated.pythonanywhere.com")
-  return drver
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://automated.pythonanywhere.com/login/")
+    return driver
 
-  # catch only temperature number
-def cleantext(text):
-  output = float(text.split(": ")[1])
-  return output
-
-
-  #specify what we are scraping from the page
 def main():
-  drver = get_drver()
-  time.sleep(2)
-  element = drver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return cleantext(element.text)
+    driver = get_drvier()
 
+    # Log in
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "id_password")))
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
 
-print(main())
+    # Wait for the redirection to the dashboard
+    WebDriverWait(driver, 10).until(EC.url_contains("/dashboard"))
+
+    # Verify the redirection to the dashboard
+    current_url = driver.current_url
+    if "dashboard" in current_url:
+        print(f"Successfully redirected to the dashboard: {current_url}")
+    else:
+        print(f"Redirection failed. Current URL: {current_url}")
+
+main()
